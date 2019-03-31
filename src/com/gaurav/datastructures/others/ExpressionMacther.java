b61a6fc13b3ce4a0d102896263f45bcafc862468
Gaurav <kgaurav9singh@gmail.com>
2019-03-31 20:26:30 +0530

    Expression matcher changes

--- src/com/gaurav/datastructures/others/ExpressionMacther.java
+++ src/com/gaurav/datastructures/others/ExpressionMacther.java
@@ -7,25 +7,60 @@ public class ExpressionMacther {
 
 	public static void main(String[] args) {
 
-		System.out.println(isMatched("()(()){([()])}"));
+		String input1 = "()(()){([()])}";
+		String input2 = "((()(()){([()])}))";
+		String input3 = ")(()){([()])}";
+		String input4 = "({[])}";
+		String input5 = "(";
 
-		System.out.println(isMatched("({[])}"));
+		System.out.println("For -> " + input1 + " " + isMatched(input1));
+
+		/*System.out.println("\n\n********************");
+
+		System.out.println("For -> " + input2 + " " + isMatched(input2));
+
+		System.out.println("\n\n********************");
+
+		System.out.println("For -> " + input3 + " " + isMatched(input3));
+
+		System.out.println("\n\n********************");
+
+		System.out.println("For -> " + input4 + " " + isMatched(input4));
+
+		System.out.println("\n\n********************");
+
+		System.out.println("For -> " + input5 + " " + isMatched(input5));*/
 
 	}
 
 	/** Tests if delimiters in the given expression are properly matched. */
 	public static boolean isMatched(String expression) {
-		final String opening = "({["; // opening delimiters
+		final String opening = "({["; // opening delimisiters
 		final String closing = ")}]"; // respective closing delimiters
 		Stack<Character> buffer = new LinkedStack<>();
+		int itr = 0;
 		for (char c : expression.toCharArray()) {
+			System.out.println("itr: " + itr++);
 			if (opening.indexOf(c) != -1) // this is a left delimiter
+			{
+				System.out.println("In first if: " + opening.indexOf(c));
 				buffer.push(c);
-			else if (closing.indexOf(c) != -1) { // this is a right delimiter
+				System.out.println("Stack contains: " + buffer.top());
+			} else if (closing.indexOf(c) != -1) { // this is a right delimiter
+				System.out.println("In else if");
 				if (buffer.isEmpty()) // nothing to match with
+				{
+					System.out.println("In else if-if");
 					return false;
-				if (closing.indexOf(c) != opening.indexOf(buffer.pop()))
+				}
+				int cind = closing.indexOf(c);
+				int oindpop = opening.indexOf(buffer.pop());
+				if (cind != oindpop) {
+					System.out.println("In last if");
+					System.out.println(closing.indexOf(c));
+					System.out.println(buffer.top());
 					return false; // mismatched delimiter
+				}
 			}
 		}
 		return buffer.isEmpty(); // were all opening delimiters matched?
